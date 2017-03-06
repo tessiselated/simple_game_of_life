c30f7ebf7d3355c73ca913b988e92a6399364f7c
Tess Elation <fredisded@gmail.com>
2017-03-06 21:18:16 +1100

    randomise initial game state

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,37 @@
 var height = 20;
 var width = 80;
+var gameState;
 
-function randomiseInitialState() {
 
-
-}
 
 // function to draw a grid in the canvas area
 
 function draw() {
+    randomizeInitialState();
     var ctx = document.getElementById('board').getContext('2d');
     for (var i = 0; i < width; i++) {
         for (var j = 0; j < height; j++) {
             ctx.strokeRect(i * 10, j * 10, 10, 10);
+            if (gameState[i][j] === true) {
+                ctx.fillRect(i * 10, j * 10, 10, 10);
+            }
         }
     }
 }
 
+
+function randomizeInitialState() {
+    gameState = new Array(20);
+    for (var i = 0; i < 80; i++) {
+        gameState[i] = new Array(80);
+        for (var j = 0; j < 20; j++) {
+            if (Math.round(Math.random()) ===1) {
+                gameState[i][j] = true;
+            } else {
+                gameState[i][j] = false;
+            }
+        }
+    }
+}
 
 draw();
